3e472c3968689605ca3e502bd64b7f750927e47f
Drashti2Sojitra <108918943+Drashti2Sojitra@users.noreply.github.com>
2022-07-08 23:35:19 +0530

    Add files via upload

---
+++ src/utils/enum.ts
@@ -0,0 +1,19 @@
+export enum RoutePaths {
+  Login = "/login",
+  Register = "/register",
+  Home = "/",
+  UpdateProfile = "UpdateProfile",
+  Book = "Book",
+  Category = "Category",
+  User = "User",
+  BookListing = "BookListing",
+  AddBook = "AddBook",
+  EditBook = "EditBook",
+  Cart = "Cart"
+}
+
+export enum Role {
+    Admin = 1,
+    Seller = 2,
+    Buyer = 3
+ }
